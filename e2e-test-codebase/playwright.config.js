8b1c4eb03873be4357a890b6aec421f088f54ff1
kkgthb <44843658+kkgthb@users.noreply.github.com>
2023-02-24 16:18:00 -0600

    Comment added - Playwright config seems optional

--- e2e-test-codebase/playwright.config.js
+++ e2e-test-codebase/playwright.config.js
@@ -1,3 +1,10 @@
+// Interesting -- I had forgotten to put this file into the proper folder and also 
+// had made a typo in it, meaning it definitely was out of commission, 
+// and Playwright still ran "./my-e2e/tests/my-first-tests.spec.js" just fine.
+// It seems that having a "playwright.config.js" file is entirely optional 
+// and the "npx playwright test" is smart enough to scan subfolders for files 
+// ending in ".spec.js" without a hint as seen below.
+
 const { defineConfig, devices } = require("@playwright/test");
 
 module.exports = defineConfig({
